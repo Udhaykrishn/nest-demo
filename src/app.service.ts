8d750f89f33987d5ef121d5002113cd714dcab9e
Udhaykrishn <krishnauday320@gmail.com>
2024-06-08 20:08:48 +0530

    added env

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly config:ConfigService){}
   getHello(): string {
-    return 'Hello World!';
+    
+    return this.config.get<string>("PORT")
   }
 }
